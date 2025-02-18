faa63371079d41b5d01c0d6c2b548bcfa92f7ac6
HyperIndexTech <j.lee17th@gmail.com>
2025-02-19 01:53:31 +0800

    fix: fix liquidity bug

--- src/app/explore/pools/[hash]/page.tsx
+++ src/app/explore/pools/[hash]/page.tsx
@@ -122,12 +122,12 @@ export default function Page() {
 
 
 						{/* Swap容器 */}
-						<div className={`mt-6 transition-all duration-300 ease-in-out ${showSwap ? 'opacity-100 max-h-[500px]' : 'opacity-0 max-h-0 overflow-hidden'}`}>
+						<div className={`mt-6 transition-all duration-300 ease-in-out ${showSwap ? 'opacity-100' : 'opacity-0 max-h-0 overflow-hidden'}`}>
 							<SwapContainer token1={pool?.token0} token2={pool?.token1} />
 						</div>
 
 						{/* 池子数据网格 */}
-						<div className="grid grid-cols-1 gap-4 mb-6">
+						<div className="grid grid-cols-1 gap-4 my-6">
 							<div className="stat bg-base-200 rounded-box p-4">
 								<div className="stat-title text-sm">APY</div>
 								<div className="stat-value text-primary text-xl">{formatNumber(pool?.APY || 0, 3)}</div>
