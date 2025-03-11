e3479c06225650d5979654882044b92f1ec8f1f2
Bevan <a>
2025-03-11 18:55:09 +0800

    fix: banner

--- src/app/explore/pools/page.tsx
+++ src/app/explore/pools/page.tsx
@@ -5,7 +5,7 @@ import Explore from '@/components/Explore'
 export default function ExplorePage() {
 	return (
 		<div className="flex justify-center items-center min-h-screen pt-14">
-			<div className="container mx-auto px-4">
+			<div className="container mx-auto">
 				<Explore activeTab={2} />
 			</div>
 		</div>
